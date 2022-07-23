97084aaa6604e9700e464f0837417496ae4836e1
Martí Bosch <marti.bosch@protonmail.com>
2022-07-23 11:23:10 +0200

    fix github setup

--- terraform/modules/env/providers.tf
+++ terraform/modules/env/providers.tf
@@ -4,17 +4,9 @@ terraform {
       source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
-    github = {
-      source  = "integrations/github"
-      version = "~> 4.0"
-    }
   }
 }
 
 provider "digitalocean" {
   token = var.do_token
-}
-
-provider "github" {
-  token = var.gh_token
 }
